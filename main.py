1a4d90707d4c4285ff1473dd1e4d2af23bdf8a86
fdelgadov <fdelgadov@unsa.edu.pe>
2024-05-15 13:58:59 -0500

    Tarea terminada

--- main.py
+++ main.py
@@ -12,7 +12,22 @@ print(txt)
 txt = a_mayuscula(txt)
 print(txt)
 txt = eliminar_signos(txt)
-print(txt)
+print(f"{txt}\nLongitud: {len(txt)}")
 
-with open("PRACTICA1_PRE.TXT", "w", encoding="utf-8") as archivo:
+archivo_destino = "PRACTICA1_PRE.TXT"
+with open(archivo_destino, "w", encoding="utf-8") as archivo:
     archivo.write(txt)
+
+tabla_frecuencias = frecuencias(archivo_destino)
+
+with open(archivo_destino, 'r', encoding="utf-8") as archivo:
+    secuencias = secuencias_repetidas(archivo.read())
+    distancias = calcular_distancias(secuencias)
+print(f"{tabla_frecuencias}\n\n{secuencias}\n\n{distancias}")
+
+with open(archivo_destino, 'r', encoding="utf-8") as archivo:
+    txt = archivo.read()
+
+print(a_unicode(txt))
+print(otra_sustitucion(txt))
+print(insertar_epis(txt))
--- preprocesamiento.py
+++ preprocesamiento.py
@@ -22,13 +22,16 @@ tildes_vocales = {
     'Ú': 'U'
 }
 
-signos = [' ', ',', '.', ';']
+signos = [' ', ',', '.', ';', '\n']
 
 def sustituciones(txt):
     aux = txt
     for k, v in caracter_sustituto.items():
         aux = aux.replace(k, v)
-        aux = aux.replace(chr(ord(k) - 32), chr(ord(v) - 32))
+        if k != 'ñ':
+            aux = aux.replace(chr(ord(k) - 32), chr(ord(v) - 32))
+        else:
+            aux = aux.replace('Ñ', 'E')
 
     return aux
 
@@ -44,13 +47,17 @@ def a_mayuscula(txt):
     for x in range(97, 123):
         aux = aux.replace(chr(x), chr(x - 32))
 
+    aux = aux.replace('ñ', 'Ñ')
+
     return aux
 
 def eliminar_signos(txt):
-    aux = txt
+    validos = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    aux = ""
 
-    for c in signos:
-        aux = aux.replace(c, '')
+    for c in txt:
+        if c in validos:
+            aux += c
 
     return aux
 
@@ -62,3 +69,62 @@ def preprocesamiento(texto):
     res = eliminar_signos(res)
 
     return res
+
+def frecuencias(archivo):
+    with open(archivo, 'r', encoding="utf-8") as archivo:
+        txt = archivo.read()
+
+    diccionaro_frecuencias = {chr(x): 0 for x in range(65, 91)}
+    diccionaro_frecuencias['Ñ'] = 0
+
+    for c in txt:
+        diccionaro_frecuencias[c] += 1
+
+    return diccionaro_frecuencias
+
+def secuencias_repetidas(texto):
+    secuencias = {}
+    for i in range(len(texto) - 3 + 1):
+        secuencia = texto[i:i + 3]
+        try:
+            secuencias[secuencia].append(i)
+        except:
+            secuencias[secuencia] = [i]
+
+    return {k: v for k, v in secuencias.items() if len(v) > 1}
+    
+def calcular_distancias(secuencias):
+    distancias = []
+    for posiciones in secuencias.values():
+        for i in range(len(posiciones) - 1):
+            distancias.append(posiciones[i + 1] - posiciones[i])
+    return distancias
+
+def a_unicode(texto):
+    aux = ""
+    for c in texto:
+        hexa = f"{hex(ord(c))}"
+        aux += f"{hexa[2:]}"
+
+    return aux
+
+def otra_sustitucion(txt):
+    aux = ""
+    for c in txt:
+        aux += chr(ord(c) + 100)
+
+    return aux
+
+def insertar_epis(txt):
+    aux = txt[0]
+
+    for i in range(1, len(txt)):
+        if i % 13 == 0:
+            aux += "EPIS"
+        aux += txt[i]
+
+    mod = len(aux) % 5
+    if mod != 0:
+        aux += "ZZZZ"[:(5 - mod)]
+
+    return aux
